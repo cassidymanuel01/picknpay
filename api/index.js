eabb1754faf8aaf01365127a3db459591c75d040
MuddathirDawood <dmuddathir@gmail.com>
2022-08-05 09:17:28 +0200

    api pt3

--- api/index.js
+++ api/index.js
@@ -5,45 +5,55 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 const db = require('./config/dbconn');
 const {compare, genSalt, hash} = require('bcrypt');
-const jwt = require('jsonwebtoken')
+const jwt = require('jsonwebtoken');
 
 // Express app
 const app = express();
+app.use(express.static('views'))
 
 // Express router
 const router = express.Router();
 
 // Configuration 
 const port = parseInt(process.env.PORT) || 4000;
-app.use(router, cors(), express.json(), express.urlencoded({ extended: true }));
+app.use(router, cors(), express.json(), bodyParser.urlencoded({ extended: true }));
 app.listen(port, ()=> {console.log(`Server is running on port ${port}`)});
 
 
 
 /* ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////// */
 // REGISTER
-router.post('/register', bodyParser.json(), async (req, res)=> {
-    const bd = req.body;
-    // Encrypting a password
-    let generateSalt = await genSalt();
-    bd.userpassword = await hash(bd.userpassword, generateSalt);
-    // Query
-
-    const strQry = 
-    `
-    INSERT INTO users(firstname, lastname, gender, address, email, userpassword)
-    VALUES(?, ?, ?, ?, ?, ?);
-    `;
-    //
-    db.query(strQry, 
-        [bd.firstname, bd.lastname, bd.gender, bd.address, bd.email, bd.userpassword],
-        (err, results)=> {
-            if(err) throw err;
-            res.send(`number of affected row/s: ${results.affectedRows}`);
-        })
-});
-
-
+router.post('/register', bodyParser.json(),(req, res)=>{
+    let emails = `SELECT email FROM users WHERE ?`;
+    let email = {
+        email: req.body.email
+    }
+    db.query(emails, email, async(err, results)=>{
+        if(err) throw err
+        // VALIDATION
+        if (results.length > 0) {
+            res.send("The provided email exists. Please enter another one");
+            
+        } else {
+            const bd = req.body;
+            let generateSalt = await genSalt();
+            bd.userpassword = await hash(bd.userpassword, generateSalt);
+            // Query
+            const strQry = 
+            `
+            INSERT INTO users(firstname, lastname, gender, email, userpassword)
+            VALUES(?, ?, ?, ?, ?);
+            `;
+            //
+            db.query(strQry, 
+                [bd.firstname, bd.lastname, bd.gender, bd.email, bd.userpassword],
+                (err, results)=> {
+                    if(err) throw err;
+                    res.send(`number of affected row/s: ${results.affectedRows}`);
+                })
+        }
+    })
+})
 
 
 /* ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////// */
@@ -69,7 +79,6 @@ router.post('/login', bodyParser.json(), (req, res)=> {
                       firstname: results[0].firstname,
                       lastname: results[0].lastname,
                       gender: results[0].gender,
-                      address: results[0].address,
                       userRole: results[0].userRole,
                       email: results[0].email,
                       userpassword: results[0].userpassword,
@@ -78,9 +87,7 @@ router.post('/login', bodyParser.json(), (req, res)=> {
 
                 jwt.sign(payload,process.env.jwtSecret,{expiresIn: "365d"},(err, token) => {
                     if (err) throw err;
-                    res.json({
-                        tokenSecret: token
-                    });
+                    res.send(token)
                   }
                 );  
             }
@@ -88,6 +95,26 @@ router.post('/login', bodyParser.json(), (req, res)=> {
 
     }) 
 })
+
+
+
+/* ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////// */
+// GET ALL USERS
+router.get('/users', (req, res)=> {
+    // Query
+    const strQry = 
+    `
+    SELECT id, firstname, lastname, gender, userRole, email
+    FROM users;
+    `;
+    db.query(strQry, (err, results)=> {
+        if(err) throw err;
+        res.json({
+            status: 200,
+            users: results
+        })
+    })
+});
 
 
 
@@ -104,8 +131,6 @@ router.get("/users/verify", (req, res) => {
       }
     });
   });
-
-
 
 
 /* ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////// */
@@ -142,9 +167,9 @@ router.get('/products', (req, res)=> {
     `;
     db.query(strQry, (err, results)=> {
         if(err) throw err;
-        res.json({
-            status: 200,
-            results: results
+        res.status(200).json({
+            status: 'ok',
+            products: results
         })
     })
 });
@@ -211,3 +236,10 @@ res.status(200).json({
     results: results
 })
 */
+
+/* ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////// */
+/* //////////////////////////////////////////////////////////// PAGES \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\ */
+
+app.get('/product', (req, res)=>{
+    res.sendFile(__dirname + "/views/products.html")
+})
